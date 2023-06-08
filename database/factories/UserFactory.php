19190b3aaa77cf3aef095b3e5b72fdc4da88a871
Naveed Shahzad <lhrciit@gmail.com>
2023-06-08 21:28:26 +0500

    working password

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,7 +19,7 @@ class UserFactory extends Factory
             'username' => $this->faker->unique()->userName(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => '$2y$10$lj.qrcRY.KMHrMcM671suuGsW6U8s2sptvuDImkc.dmm2gWje5qK.', // 12345678
             'remember_token' => Str::random(10),
         ];
     }
