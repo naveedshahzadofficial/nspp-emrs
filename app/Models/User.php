4085f94b4de0e86858fdad9dab6ea0e49cd98b73
Naveed Shahzad <lhrciit@gmail.com>
2023-06-08 21:14:37 +0500

    working on auth

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,10 +45,6 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function getAuthIdentifierName(): string
-    {
-        return 'username';
-    }
 
     public function scopeActive($query) {
         return $query->where('status', true);
