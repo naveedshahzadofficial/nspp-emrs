c5a9ae42ef1689496283ec9b76794a3d5a9ccd97
Naveed Shahzad <lhrciit@gmail.com>
2023-10-05 18:15:53 +0500

    participant service

--- app/Services/ParticipantService.php
+++ app/Services/ParticipantService.php
@@ -12,18 +12,28 @@ class ParticipantService
 
     public function __construct()
     {
-        $this->api_base_url = config('hrmis.api_base_url');
-        $this->auth_key = config('hrmis.auth_key');
+        $this->api_base_url = config('nsppeportal.api_base_url');
+        $this->auth_key = config('nsppeportal.auth_key');
     }
 
     public function getParticipants($institute_id)
     {
         $client = new Client();
-        $request = new Request('POST', $this->api_base_url.'/get_officer_data',
+        $request = new Request('POST', $this->api_base_url.'/get_institute_participant_list',
             [
                 'Content-Type' => 'application/json',
-                'Auth-key' => $this->auth_key,
-            ],json_encode(['institute_id'=> $institute_id]));
+            ],[
+                'multipart' => [
+                    [
+                        'name' => 'auth_key',
+                        'contents' => $this->auth_key
+                    ],
+                    [
+                        'name' => 'institute_id',
+                        'contents' => $institute_id
+                    ]
+                ]
+            ]);
         $response = $client->sendAsync($request)->wait();
         $response_json =  $response->getBody()->getContents();
         $obj = json_decode(trim($response_json));
---
+++ config/nsppeportal.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'api_base_url' => env('NSPPEPORTAL_BASE_URL', 'https://nsppeportal.pitb.gov.pk/home'),
+    'auth_key' => env('NSPPEPORTAL_AUTH_KEY', 'nsppliveportal'),
+];
